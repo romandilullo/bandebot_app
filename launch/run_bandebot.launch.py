f157c300020348febfc2730c0f3c929b5c7dac58
romandilullo <romandilullo@gmail.com>
2025-11-06 15:36:09 -0300

    .

--- launch/run_bandebot.launch.py
+++ launch/run_bandebot.launch.py
@@ -30,8 +30,23 @@ def generate_launch_description():
         output='screen'
     )
 
+    head_display_node = Node(
+            package='bandebot_display_controller',
+            executable='display_controller_node',
+            name='display_controller',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/display_controller',
+                'serial_baudrate': 115200,
+            }],
+            remappings=[
+                ('display/text', '/bandebot/display/text'),
+            ]
+        )
+
     return LaunchDescription([
         mulita_launch,
         bandebot_node,
+        # head_display_node,
         catering_node
     ])
